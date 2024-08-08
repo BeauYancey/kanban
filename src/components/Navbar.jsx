6320260666008ef31fab4b387f251c6e0d9399f5
Beau Yancey <yancey.beau.j@gmail.com>
2024-08-07 20:59:20 -0600

    added nav/boards dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,14 @@ export default function Navbar() {
 	return (
 		<nav>
 			<Link to='/'><h2>kanban</h2></Link>
-			<Link to='/boards'><h3>boards</h3></Link>
+			<div className='nav-drop'>
+				<h3>boards</h3>
+				<div className='nav-drop-content'>
+					<Link to='/board/1'><h4>Homework</h4></Link>
+					<Link to='/board/2'><h4>Bathroom Renovation</h4></Link>
+					<button className='btn' style={{cursor: 'pointer'}} onClick={() => console.log('clicked')}><h3>Create +</h3></button>
+				</div>
+			</div>
 		</nav>
 	)
 }
